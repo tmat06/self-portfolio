87ef165ed287e0c281504fa66d7e93da5ffd6c7c
tmat06 <timmy.a.matthews@gmail.com>
2018-07-13 20:17:53 -0600

    hover is reacting on display pictures

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,18 +4,37 @@ import ProjectDisplay from "./ProjectDisplay";
 export default class Project extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      project1: {
+        name: "Restaurant Roulette",
+        pictures: {
+          pic1: "/images/Title-Screen.PNG",
+          pic2: "/images/Results.PNG",
+          pic3: "/images/Profile.PNG",
+          pic4: "/images/google-maps.PNG"
+        },
+        description: "This is my project"
+      }
+    };
   }
 
   render() {
     return (
       <div className="project">
+        <div className="project-3-row">
+          <ProjectDisplay project={this.state.project1} />
+          <ProjectDisplay project={this.state.project1} />
+          <ProjectDisplay project={this.state.project1} />
+        </div>
+        <div className="project-3-row">
+          <ProjectDisplay project={this.state.project1} />
+          <ProjectDisplay project={this.state.project1} />
+          <ProjectDisplay project={this.state.project1} />
+        </div>
+        {/* <ProjectDisplay />
         <ProjectDisplay />
         <ProjectDisplay />
-        <ProjectDisplay />
-        <ProjectDisplay />
-        <ProjectDisplay />
-        <ProjectDisplay />
+        <ProjectDisplay /> */}
       </div>
     );
   }
